7ebe7e9d646ec73ccdc81ca555480049e7c6bcff
pawoo0211 <pawoo0211@naver.com>
2024-11-30 23:38:36 +0900

    샘플 테스트 코드 작성

---
+++ apps/proxy/qna/src/test/java/domain/AnswerTest.java
@@ -0,0 +1,13 @@
+package domain;
+
+
+import com.example.domain.Answer;
+
+public class AnswerTest {
+    /**
+     * ToDO
+     * - 멀티 모듈 의존성 확인 후 Fixture 재구성
+     */
+//    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+//    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+}
---
+++ apps/proxy/qna/src/test/java/domain/QuestionTest.java
@@ -0,0 +1,13 @@
+package domain;
+
+
+import com.example.domain.Question;
+
+public class QuestionTest {
+    /**
+     * ToDO
+     * - 멀티 모듈 의존성 확인 후 Fixture 재구성
+     */
+//    public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+//    public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+}
