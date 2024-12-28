f7dc5bde0259379e7c5d4e02a910662cd69d67c0
pawoo0211 <pawoo0211@naver.com>
2024-12-28 12:04:43 +0900

    결제 모듈 - PG 요청 로직 리팩터링 #8

--- apps/proxy/payment/src/main/java/com/example/framework/adapter/out/network/PaymentGatewayNetworkAdapter.java
+++ apps/proxy/payment/src/main/java/com/example/framework/adapter/out/network/PaymentGatewayNetworkAdapter.java
@@ -4,7 +4,7 @@ import com.example.application.port.out.PaymentGatewayOutputPort;
 import com.example.framework.adapter.out.network.response.GraphQLCancelResponse;
 import com.example.framework.adapter.out.network.response.GraphQLRequestPaymentResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,90 +19,83 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class PaymentGatewayNetworkAdapter implements PaymentGatewayOutputPort {
     private final RestTemplate restTemplate;
 
-    public PaymentGatewayNetworkAdapter(final RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
     @Override
     public GraphQLRequestPaymentResponse.RequestPaymentResponseData requestPayment(
-            String paymentId, String merchantId, BigDecimal price)
-    {
-        // GraphQL 쿼리 및 변수 설정
-        String query = PaymentGatewayInfo.CREATE_PAYMENT_GRAPHQL_QUERY.value;
-
+            String paymentId, String merchantId, BigDecimal price
+    ) {
         // 요청 변수 설정
-        Map<String, Object> variables = new HashMap<>();
         Map<String, Object> request = new HashMap<>();
         request.put("orderId", paymentId);
         request.put("merchantId", merchantId);
         request.put("price", price);
-        variables.put("request", request);
 
         // 요청 본문 구성
         Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("query", query);
-        requestBody.put("variables", variables);
+        requestBody.put("query", PaymentGatewayInfo.CREATE_PAYMENT_GRAPHQL_QUERY.value);
+        requestBody.put("variables", createVariables(request));
 
-        // 요청 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // 요청 본문을 포함한 HTTP 엔티티 생성
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
-
-        // GraphQL 서버에 POST 요청 보내기
-        URI url = UriComponentsBuilder.fromUriString(PaymentGatewayInfo.PG_END_POINT.value)
-                .build()
-                .toUri();
         GraphQLRequestPaymentResponse response = restTemplate.exchange(
-                url, HttpMethod.POST, entity, GraphQLRequestPaymentResponse.class
+                fromPGUrl(),
+                HttpMethod.POST,
+                createEntity(requestBody),
+                GraphQLRequestPaymentResponse.class
         ).getBody();
 
-        // 응답 처리 (결과에 따른 로직 추가)
         System.out.println("PG 결제 응답 : " + response);
-
         return response.getData().getCreatePayment();
     }
 
     @Override
-    public GraphQLCancelResponse.CancelResponseData requestCancel(String paymentId, String merchantId) {
-        // GraphQL 쿼리 및 변수 설정
-        String query = PaymentGatewayInfo.CREATE_CANCEL_GRAPHQL_QUERY.value;
-
+    public GraphQLCancelResponse.CancelResponseData requestCancel(
+            String paymentId,
+            String merchantId
+    ) {
         // 요청 변수 설정
-        Map<String, Object> variables = new HashMap<>();
         Map<String, Object> request = new HashMap<>();
         request.put("orderId", paymentId);
         request.put("merchantId", merchantId);
-        variables.put("request", request);
 
         // 요청 본문 구성
         Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("query", query);
-        requestBody.put("variables", variables);
+        requestBody.put("query", PaymentGatewayInfo.CREATE_CANCEL_GRAPHQL_QUERY.value);
+        requestBody.put("variables", createVariables(request));
 
+        // GraphQL 서버에 POST 요청 보내기
+        GraphQLCancelResponse response = restTemplate.exchange(
+                fromPGUrl(),
+                HttpMethod.POST,
+                createEntity(requestBody),
+                GraphQLCancelResponse.class
+        ).getBody();
+
+        System.out.println("PG 결제 응답 : " + response);
+        return response.getData().getCancelPayment();
+    }
+
+    private Map<String, Object> createVariables(Map<String, Object> request) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("request", request);
+        return variables;
+    }
+
+    private HttpEntity<Map<String, Object>> createEntity(
+            Map<String, Object> requestBody
+    ) {
         // 요청 헤더 설정
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // 요청 본문을 포함한 HTTP 엔티티 생성
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+        return new HttpEntity<>(requestBody, headers);
+    }
 
-        // GraphQL 서버에 POST 요청 보내기
-        URI url = UriComponentsBuilder.fromUriString(PaymentGatewayInfo.PG_END_POINT.value)
+    private URI fromPGUrl() {
+        return UriComponentsBuilder.fromUriString(PaymentGatewayInfo.PG_END_POINT.value)
                 .build()
                 .toUri();
-        GraphQLCancelResponse response = restTemplate.exchange(
-                url, HttpMethod.POST, entity, GraphQLCancelResponse.class
-        ).getBody();
-
-        // 응답 처리 (결과에 따른 로직 추가)
-        System.out.println("PG 결제 응답 : " + response);
-
-        return response.getData().getCancelPayment();
     }
 
     @AllArgsConstructor
